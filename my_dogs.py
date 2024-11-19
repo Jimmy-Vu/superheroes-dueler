62f4477784ffec7abf4b0a5ffa2425e9e5705c39
Jimmy Vu <JMV3211@gmail.com>
2024-11-18 21:16:18 -0800

    created dog class

---
+++ my_dogs.py
@@ -0,0 +1,13 @@
+from dog import Dog
+
+my_dog = Dog("Rex", "SuperDog")
+my_dog.bark()
+
+my_other_dog = Dog("Annie", "SuperDog")
+print(my_other_dog.name)
+
+my_third_dog = Dog("Oliver", "HotDog")
+
+my_other_dog.bark()
+my_third_dog.sit()
+my_dog.rollover()
